eb61778d5ac73c7c7848a63f41e5844fb236da99
Kris Nee <kristi.neemesto@gmail.com>
2024-10-07 13:45:10 +0300

    Kristi kommentaar

---
+++ FoodInspo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FoodInspo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
